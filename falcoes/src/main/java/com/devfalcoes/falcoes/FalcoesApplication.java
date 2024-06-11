395541f4ea53f305cd8646cf76d368c46715c1c6
Lucas Ferreira Lourenço <141689321+LucasFL05@users.noreply.github.com>
2024-06-11 20:23:03 -0300

    correção

---
+++ falcoes/src/main/java/com/devfalcoes/falcoes/FalcoesApplication.java
@@ -0,0 +1,13 @@
+package com.devfalcoes.falcoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalcoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalcoesApplication.class, args);
+	}
+
+}
